7a2127d58d8164e1c0d873cf58a995e8154a56f1
grehochka82 <lobodina0607@mail.ru>
2026-04-21 10:50:52 +0300

    Added complete README for Lab18

--- main.js
+++ main.js
@@ -48,28 +48,83 @@
 // let numbers = [1,2,3,4,5];
 // let mixed = ["текст", 42, true, null];
 
-function sum (a,b){
-    return a+b;
+// function sum (a,b){
+//     return a+b;
+// }
+// let multiply = function (x,y){
+//     return x*y;
+// };
+// console.log(sum(5, 3));
+
+// let now = new Date();
+// let birthday = new Date("1995-12-17");
+
+// let a= 10;
+// let b =3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(10+"5");
+// console.log("10"-5);
+
+// const numbersArray =[1,2,3];
+// numbersArray[0] =10;
+// console.log(numbersArray);
+
+let yourAge =18;
+
+if(yourAge >=18){
+    console.log("Доступ разрешен");
+}else{
+    console.log("Доступ запрещен");
 }
-let multiply = function (x,y){
-    return x*y;
-};
-console.log(sum(5, 3));
 
-let now = new Date();
-let birthday = new Date("1995-12-17");
+let temperature = 25;
+let isSunny = true;
+if (temperature > 20 && isSunny) {
+    console.log("Отличная погода для прогулки!");
+}
 
-let a= 10;
-let b =3;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+if(isStudent && age <25){
+    console.log("Доступна студенческа скидка");
+}
 
-console.log(10+"5");
-console.log("10"-5);
+let isLoggedIn = true;
+let isAdmin = true;
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченный доступ");
+} else {
+    console.log("Доступ запрещён");
+}
 
-const numbersArray =[1,2,3];
-numbersArray[0] =10;
-console.log(numbersArray);
-//numbersArray= [5,6,7];
+let message=age>=18?"Совершеннолетний":"Несовешеннолетний";
+console/log("message");
+
+let day =3;
+switch(day){
+    case 1: console.log("Понедельник"); break;
+    case 2: console.log("Вторник"); break;
+    case 3: console.log("Среда"); break;
+    default: console.log("Неизвестный день");
+}
+
+let monthNumber = 7;
+switch (monthNumber) {
+    case 1: console.log("Январь"); break;
+    case 2: console.log("Февраль"); break;
+    case 3: console.log("Март"); break;
+    case 4: console.log("Апрель"); break;
+    case 5: console.log("Май"); break;
+    case 6: console.log("Июнь"); break;
+    case 7: console.log("Июль"); break;
+    case 8: console.log("Август"); break;
+    case 9: console.log("Сентябрь"); break;
+    case 10: console.log("Октябрь"); break;
+    case 11: console.log("Ноябрь"); break;
+    case 12: console.log("Декабрь"); break;
+    default: console.log("Ошибка!");
+}
